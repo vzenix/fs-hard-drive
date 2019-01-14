332671351e5d38a7860eeaded945d71a3895e53b
Francisco M. E <paco@vzenix.es>
2019-01-14 21:29:29 +0100

    init ls device method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-
-const exec = require('child_process').exec,
+const {
+    exec
+} = require('child_process');
 const os = require('os');
-const fs = require('fs');
+const {
+    CMDformatResponseSpaces
+} = require('./utils');
 
-export.Utils = Utils;
-export.Disk = Disk;
+exports.DriveType = {
+    Unknown: 0,
+    NoRootDirectory: 1,
+    RemovableDisk: 2,
+    LocalDisk: 3,
+    NetworkDrive: 4,
+    CompactDisc: 5,
+    RAMDisk: 6
+};
 
-function Utils() {}
+exports.lsDevices = lsDevices;
 
-function Disk() {}
+const isWindows = os.type().indexOf('Window') >= 0;
+const isLinux = os.type().indexOf('Linux') >= 0;
+const isMac = os.type().indexOf('Darwin') >= 0;
 
-console.log("TEST CASE");
+function lsDevices(fn) {
+    if (isWindows) {
+        return lsDevicesWindows(fn);
+    }
+
+    if (isLinux || isMac) {
+        return lsDevicesDF(fn);
+    }
+
+    throw `OS ${os.type()} not supported`;
+}
+
+function lsDevicesWindows(fn) {
+    exec('wmic logicaldisk', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+
+        let info = CMDformatResponseSpaces(stdout);
+        fn(info);
+    });
+}
+
+// lsDevices((r) => console.log('Response: ', r));
+//
---
+++ test/specs/demo/ls-devices.spec.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const {
+    lsDevices
+} = require('../../../src/index');
+
+describe('lsDevince', () => {
+    it('Test 01:', () => {
+        // lsDevices((res) => console.log('Response: ', res));
+        assert.equal(1, 1);
+    });
+});
